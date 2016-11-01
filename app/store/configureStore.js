c0ac3fd7aeba455b6970b0d8499b33736a808905
AlexLau0907 <yuzhiliu0907@126.com>
2016-11-01 15:20:25 +0800

    Add redux middleware thunk promise logger

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,8 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
 
+const logger = createLogger();
+
 export default  configureStore = () => {
-  const store = createStore(rootReducer);
+  const store = createStore(rootReducer, applyMiddleware(thunk, promise, logger));
   if (module.hot) {
     module.hot.accept(() => {
       const nextRootReducer = require('../reducers/index').default
